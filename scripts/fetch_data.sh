ddef95202d793e05a94234ce824c868362ceaf5b
Andrew Berger <andbberger@gmail.com>
2018-03-20 17:28:27 -0400

    tweak script

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# fetches data from dropbox and puts in ~/data/klb
+# fetches data from dropbox and puts in ~/data/divison_detection/projections
 
+cd /tmp
 wget https://www.dropbox.com/s/lvuoq22joc64lul/Volumes.zip?dl=0#
 mv Volumes.zip\?dl\=0 vols.zip
 unzip vols.zip
-mkdir klb
-mkdir -p ~/data/klb
-mv Volume_*.klb ~/data/klb/
+mkdir -p ~/data/division_detection/klb
+mkdir -p ~/data/division_detection/projections
+mv Volume_*.projection.klb ~/data/division_detection/projections
+mv Volume_*.klb ~/data/division_detection/klb
