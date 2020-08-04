49494697979f8c4abced0cf564eb332976676f4d
Andrew Berger <andbberger@gmail.com>
2020-08-03 22:44:51 -0700

    use logging's string formatting

--- division_detection/predict.py
+++ division_detection/predict.py
@@ -516,11 +516,11 @@ def single_tp_nonblocking_predict_general(model, predictions_name, in_dir, t_pre
     if not os.path.exists(pred_dir):
         os.makedirs(pred_dir)
 
-    log_path = '/var/log/division_detection'
+    log_path = '/var/log/division_detection.log'
 
     log_name = predictions_name + str(t_predict)
     logger = setup_logging(log_name, log_path)
-    logger.info("Starting predictions at {}".format(t_predict))
+    logger.info("Starting predictions at %s", t_predict)
 
     h5_dir = in_dir + '/h5'
     ex_vol_path = '{}/{}'.format(h5_dir, os.listdir(h5_dir)[0])
